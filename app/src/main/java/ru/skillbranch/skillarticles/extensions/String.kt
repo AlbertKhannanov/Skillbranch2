cc39d6a97ec3ee3de54766cf8cec222a0406e746
Albert <xannanov.albert@mail.ru>
2021-11-22 09:33:05 +0300

    change lowercase

--- app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/extensions/String.kt
@@ -6,8 +6,8 @@ fun String?.indexesOf(
 ): List<Int> {
     this ?: return listOf()
     if (this.isEmpty() || substr.isEmpty()) return listOf()
-    val tempSubstr = if (ignoreCase) substr.lowercase() else substr
-    val tempText = if (ignoreCase) this.lowercase() else this
+    val tempSubstr = if (ignoreCase) substr.toLowerCase() else substr
+    val tempText = if (ignoreCase) this.toLowerCase() else this
     return arrayListOf<Int>().apply {
         Regex(tempSubstr).findAll(tempText).forEach {
             add(it.range.first)
